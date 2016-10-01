3543831d0d52074fd8c2419c2a212ac476557f7a
Viyom <viyommittal@gmail.com>
2016-10-01 13:27:40 -0700

    Updated default parameter values and removed the commented code

--- src/traffic-control/model/pi-queue-disc.cc
+++ src/traffic-control/model/pi-queue-disc.cc
@@ -54,7 +54,7 @@ TypeId PiQueueDisc::GetTypeId (void)
                                     Queue::QUEUE_MODE_PACKETS, "QUEUE_MODE_PACKETS"))
     .AddAttribute ("MeanPktSize",
                    "Average of packet size",
-                   UintegerValue (1000),
+                   UintegerValue (500),
                    MakeUintegerAccessor (&PiQueueDisc::m_meanPktSize),
                    MakeUintegerChecker<uint32_t> ())
     .AddAttribute ("QueueRef",
@@ -64,12 +64,12 @@ TypeId PiQueueDisc::GetTypeId (void)
                    MakeUintegerChecker<uint32_t> ())
     .AddAttribute ("A",
                    "Value of alpha",
-                   DoubleValue (0.125),
+                   DoubleValue (0.00001822),
                    MakeDoubleAccessor (&PiQueueDisc::m_a),
                    MakeDoubleChecker<double> ())
     .AddAttribute ("B",
                    "Value of beta",
-                   DoubleValue (1.25),
+                   DoubleValue (0.00001816),
                    MakeDoubleAccessor (&PiQueueDisc::m_b),
                    MakeDoubleChecker<double> ())
     .AddAttribute ("W",
@@ -77,36 +77,11 @@ TypeId PiQueueDisc::GetTypeId (void)
                    DoubleValue (170),
                    MakeDoubleAccessor (&PiQueueDisc::m_w),
                    MakeDoubleChecker<double> ())
-  /*  .AddAttribute ("Tupdate",
-                   "Time period to calculate drop probability",
-                   TimeValue (Seconds (0.03)),
-                   MakeTimeAccessor (&PiQueueDisc::m_tUpdate),
-                   MakeTimeChecker ())
-    .AddAttribute ("Supdate",
-                   "Start time of the update timer",
-                   TimeValue (Seconds (0)),
-                   MakeTimeAccessor (&PiQueueDisc::m_sUpdate),
-                   MakeTimeChecker ())*/
     .AddAttribute ("QueueLimit",
                    "Queue limit in bytes/packets",
                    UintegerValue (25),
                    MakeUintegerAccessor (&PiQueueDisc::SetQueueLimit),
                    MakeUintegerChecker<uint32_t> ())
-    /*  .AddAttribute ("DequeueThreshold",
-                     "Minimum queue size in bytes before dequeue rate is measured",
-                     UintegerValue (10000),
-                     MakeUintegerAccessor (&PiQueueDisc::m_dqThreshold),
-                     MakeUintegerChecker<uint32_t> ())
-      .AddAttribute ("QueueDelayReference",
-                     "Desired queue delay",
-                     TimeValue (Seconds (0.02)),
-                     MakeTimeAccessor (&PiQueueDisc::m_qDelayRef),
-                     MakeTimeChecker ())
-      .AddAttribute ("MaxBurstAllowance",
-                     "Current max burst allowance in seconds before random drop",
-                     TimeValue (Seconds (0.1)),
-                     MakeTimeAccessor (&PiQueueDisc::m_maxBurst),
-                     MakeTimeChecker ())   */
   ;
 
   return tid;
@@ -117,7 +92,7 @@ PiQueueDisc::PiQueueDisc ()
 {
   NS_LOG_FUNCTION (this);
   m_uv = CreateObject<UniformRandomVariable> ();
-  m_rtrsEvent = Simulator::Schedule ((Time)(1.0/m_w), &PiQueueDisc::CalculateP, this);
+  m_rtrsEvent = Simulator::Schedule (Time (Seconds (1.0 / m_w)), &PiQueueDisc::CalculateP, this);
 }
 
 PiQueueDisc::~PiQueueDisc ()
@@ -233,16 +208,10 @@ PiQueueDisc::DoEnqueue (Ptr<QueueDiscItem> item)
 void
 PiQueueDisc::InitializeParams (void)
 {
-  // Initially queue is empty so variables are initialize to zero except m_dqCount
-  // m_inMeasurement = false;
-  // m_dqCount = -1;
   m_dropProb = 0;
- // m_avgDqRate = 0.0;
-  // m_dqStart = 0;
-//  m_burstState = NO_BURST;
-// m_qDelayOld = Time (Seconds (0));
   m_stats.forcedDrop = 0;
   m_stats.unforcedDrop = 0;
+  m_qOld = 0;
 }
 
 bool PiQueueDisc::DropEarly (Ptr<QueueDiscItem> item, uint32_t qSize)
@@ -268,123 +237,22 @@ bool PiQueueDisc::DropEarly (Ptr<QueueDiscItem> item, uint32_t qSize)
 void PiQueueDisc::CalculateP ()
 {
   NS_LOG_FUNCTION (this);
-//  Time qDelay;
   double p = 0.0;
   uint32_t qlen = GetQueueSize ();
   if (GetMode () == Queue::QUEUE_MODE_BYTES)
-     {
-       p = m_a*((qlen*1.0/m_meanPktSize) - m_qRef) - m_b*((m_qOld*1.0/m_meanPktSize) - m_qRef) + m_dropProb;
-     }
-   else
-     {
-       p = m_a*(qlen - m_qRef) - m_b*(m_qOld - m_qRef) + m_dropProb;
-     }
-   p = (p < 0) ? 0 : p;
-   p = (p > 1) ? 1 : p;
-   
-   m_dropProb = p;
-   m_qOld = qlen;
-   m_rtrsEvent = Simulator::Schedule ((Time)(1.0/m_w), &PiQueueDisc::CalculateP, this);
-  /* bool missingInitFlag = false;
-   if (m_avgDqRate > 0)
-     {
-       qDelay = Time (Seconds (GetInternalQueue (0)->GetNBytes () / m_avgDqRate));
-     }
-   else
-     {
-       qDelay = Time (Seconds (0));
-       missingInitFlag = true;
-     }*/
-
-//  m_qDelay = qDelay;
-
-/*  if (m_burstAllowance.GetSeconds () > 0)
     {
-      m_dropProb = 0;
+      p = m_a * ((qlen * 1.0 / m_meanPktSize) - m_qRef) - m_b * ((m_qOld * 1.0 / m_meanPktSize) - m_qRef) + m_dropProb;
     }
   else
     {
-      p = m_a * (qDelay.GetSeconds () - m_qDelayRef.GetSeconds ()) + m_b * (qDelay.GetSeconds () - m_qDelayOld.GetSeconds ());
-      if (m_dropProb < 0.001)
-        {
-          p /= 32;
-        }
-      else if (m_dropProb < 0.01)
-        {
-          p /= 8;
-        }
-      else if (m_dropProb < 0.1)
-        {
-          p /= 2;
-        }
-      else if (m_dropProb < 1)
-        {
-          p /= 0.5;
-        }
-      else if (m_dropProb < 10)
-        {
-          p /= 0.125;
-        }
-      else
-        {
-          p /= 0.03125;
-        }
-      if ((m_dropProb >= 0.1) && (p > 0.02))
-        {
-          p = 0.02;
-        }
-    }*/
+      p = m_a * (qlen - m_qRef) - m_b * (m_qOld - m_qRef) + m_dropProb;
+    }
+  p = (p < 0) ? 0 : p;
+  p = (p > 1) ? 1 : p;
 
-  // For non-linear drop in prob
-
-  /* if (qDelay.GetSeconds () == 0 && m_qDelayOld.GetSeconds () == 0)
-     {
-       p *= 0.98;
-     }
-   else if (qDelay.GetSeconds () > 0.2)
-     {
-       p += 0.02;
-     }*/
-
-  /* if (m_burstAllowance < m_tUpdate)
-     {
-       m_burstAllowance =  Time (Seconds (0));
-     }
-   else
-     {
-       m_burstAllowance -= m_tUpdate;
-     }
-
-   uint32_t burstResetLimit = BURST_RESET_TIMEOUT / m_tUpdate.GetSeconds ();
-   if ( (qDelay.GetSeconds () < 0.5 * m_qDelayRef.GetSeconds ()) && (m_qDelayOld.GetSeconds () < (0.5 * m_qDelayRef.GetSeconds ())) && (m_dropProb == 0) && !missingInitFlag )
-     {
-       m_dqCount = -1;
-       m_avgDqRate = 0.0;
-     }
-   if ( (qDelay.GetSeconds () < 0.5 * m_qDelayRef.GetSeconds ()) && (m_qDelayOld.GetSeconds () < (0.5 * m_qDelayRef.GetSeconds ())) && (m_dropProb == 0) && (m_burstAllowance.GetSeconds () == 0))
-     {
-       if (m_burstState == IN_BURST_PROTECTING)
-         {
-           m_burstState = IN_BURST;
-           m_burstReset = 0;
-         }
-       else if (m_burstState == IN_BURST)
-         {
-           m_burstReset++;
-           if (m_burstReset > burstResetLimit)
-             {
-               m_burstReset = 0;
-               m_burstState = NO_BURST;
-             }
-         }
-     }
-   else if (m_burstState == IN_BURST)
-     {
-       m_burstReset = 0;
-     }
-
-   m_qDelayOld = qDelay;
-  m_rtrsEvent = Simulator::Schedule (m_tUpdate, &PiQueueDisc::CalculateP, this);*/
+  m_dropProb = p;
+  m_qOld = qlen;
+  m_rtrsEvent = Simulator::Schedule (Time (Seconds (1.0 / m_w)), &PiQueueDisc::CalculateP, this);
 }
 
 Ptr<QueueDiscItem>
@@ -399,56 +267,6 @@ PiQueueDisc::DoDequeue ()
     }
 
   Ptr<QueueDiscItem> item = StaticCast<QueueDiscItem> (GetInternalQueue (0)->Dequeue ());
-  // double now = Simulator::Now ().GetSeconds ();
-//  uint32_t pktSize = item->GetPacketSize ();
-
-  // if not in a measurement cycle and the queue has built up to dq_threshold,
-  // start the measurement cycle
-
-/*  if ( (GetInternalQueue (0)->GetNBytes () >= m_dqThreshold) && (!m_inMeasurement) )
-    {
-      m_dqStart = now;
-      m_dqCount = 0;
-      m_inMeasurement = true;
-    }
-
-  if (m_inMeasurement)
-    {
-      m_dqCount += pktSize;
-
-      // done with a measurement cycle
-      if (m_dqCount >= m_dqThreshold)
-        {
-
-          double tmp = now - m_dqStart;
-
-          if (tmp > 0)
-            {
-              if (m_avgDqRate == 0)
-                {
-                  m_avgDqRate = m_dqCount / tmp;
-                }
-              else
-                {
-                  m_avgDqRate = (0.5 * m_avgDqRate) + (0.5 * (m_dqCount / tmp));
-                }
-            }
-
-          // restart a measurement cycle if there is enough data
-          if (GetInternalQueue (0)->GetNBytes () > m_dqThreshold)
-            {
-              m_dqStart = now;
-              m_dqCount = 0;
-              m_inMeasurement = true;
-            }
-          else
-            {
-              m_dqCount = 0;
-              m_inMeasurement = false;
-            }
-        }
-    }*/
-
   return item;
 }
 
